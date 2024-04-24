c58b5f6569f53d4ba3d21c9b8741e042cc9af1c7
linyang <lin772207039@gmail.com>
2024-04-24 16:45:46 +0800

    基础搭建中...

---
+++ src/models/file_model.py
@@ -0,0 +1,12 @@
+from src.core.extensions import db
+#from flask_login import UserMixin
+from sqlalchemy import Column,String,Integer,Text,Boolean, DateTime,BigInteger
+
+# from backend.models.system import BasicMode
+
+class FileInfo(db.Model):
+    __tablename__ = 'sys_file_info'
+    id = Column(Integer, primary_key=True)
+    file_path = Column(String(255))
+    orginname = Column(String(255))
+    localname = Column(String(255))
